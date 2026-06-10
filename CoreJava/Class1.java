2f8cd099ab6787a3000d4b3d371b4d533ca4a465
Pranav Arora <pranavarora.in@gmail.com>
2026-06-10 09:38:58 +0530

    Class 1(Basics)

---
+++ CoreJava/Class1.java
@@ -0,0 +1,31 @@
+public class Class1 {
+    public static void main(String[] args) {
+
+        /*
+        System.out.println("Hello World");
+        */
+        
+        /*
+          int num = 3;
+          System.out.println(num);
+         */
+
+        /*
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+        */
+
+        /*
+         * float marks = 6.5f;
+         * double nums = 5.66;
+         * boolean b = true;
+         * byte by = 12;
+         * long high = 5432l;
+         * short sh = 445;
+         * char c = 'k';
+         * boolean bo = true;
+         */
+    }
+}
