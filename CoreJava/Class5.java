1bc2801d7615a466092c520a937f0bbdadf41f7c
Pranav Arora <pranavarora.in@gmail.com>
2026-06-13 15:39:35 +0530

    Added upto OOPS

--- CoreJava/Class5.java
+++ CoreJava/Class5.java
@@ -2,10 +2,22 @@ public class Class5{
     public static void main(String[] args) {
         //repeat statement
         // loop - for,do-while,while
-        int i = 1;
-        while(i<5){
-            System.out.println("Hi"+ 5);
-            i++;
+        // int i = 1;
+        // while(i<5){
+        //     System.out.println("Hi"+ i);
+        //     i++;
+        // }
+        // System.out.println("Bye" + i);
+
+//        int i = 5;
+//        do{
+//            System.out.println("Hi");
+//        } while (i <= 4);
+//    }
+        for(int i=1;i<=5;i++){
+            System.out.println("DAY " + i);
+            for(int j = 9;j<=6;j++){
+                System.out.println(j);
+            }
         }
-    }
 }
