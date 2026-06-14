c17e92fc6823ee910b33878791f44bf16c3fb83c
Pranav Arora <pranavarora.in@gmail.com>
2026-06-14 17:18:29 +0530

    Added Class8.java

---
+++ CoreJava/Class8.java
@@ -0,0 +1,53 @@
+public class Class8{
+    public static void main(String[] args) {
+//        int[] nums = {3,7,2,4};
+//        //System.out.println(nums[0]);
+//        nums[1]=6;
+//        //System.out.println(nums[1]);
+//        for(int i=0;i<nums.length;i++){
+//            System.out.print(nums[i]+" ");
+//        }
+        //int random = 0;
+        //System.out.println(random);
+//        int[][] nums = new int[3][4];
+//        for(int i = 0; i < nums.length; i++ ){
+//            for(int j = 0; j < nums[0].length; j++ ){
+//                nums[i][j]=(int)(Math.random()*10);
+//                System.out.print(nums[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//        for(int[] n : nums){
+//            for(int m : n){
+//                System.out.print(m + " ");
+//            }
+//            System.out.println();
+//        }
+
+//        int[][] nums = new int[3][]; // jagged array
+//        nums[0]=new int[3];
+//        nums[1]=new int[4];
+//        nums[2]=new int[2];
+
+//        int[][] nums = new int[3][];
+//        nums[0]=new int[3];
+//        nums[1]=new int[4];
+//        nums[2]=new int[2];
+//
+//        for(int i=0;i<nums.length;i++){
+//            for(int j=0;j<nums[i].length;j++){
+//                nums[i][j]=(int)(Math.random()*10);
+//            }
+//        }
+//
+//        for(int[] n: nums){
+//            for(int m : n){
+//                System.out.print(m + " ");
+//            }
+//            System.out.println();
+//        }
+
+    //int[] nums = new int[4];
+        
+    }
+}
